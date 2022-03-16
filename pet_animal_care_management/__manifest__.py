cbe7d854f855825054efaadee6399b4fab361cc9
bilbonet <jesus@bilbonet.net>
2022-03-16 17:57:33 +0100

    [MIG][14.0] standard migration

--- pet_animal_care_management/__manifest__.py
+++ pet_animal_care_management/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Pet Animal Care Management',
-    'version': '13.0.1.0.0',
+    'version': '14.0.1.0.0',
     'category': 'Sales',
     'license': 'AGPL-3',
     'author': 'Jesus Ramiro (Bilbonet.NET)',
