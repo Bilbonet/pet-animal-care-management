5591dab2929e0618c61020c2e32e48f55da9a2d9
bilbonet <jesus@bilbonet.net>
2022-04-13 09:58:16 +0200

    [FIX] Change veterinarian option form hr_employee model to the new abstract model hr.employee.base

--- pet_animal_care_management/models/__init__.py
+++ pet_animal_care_management/models/__init__.py
@@ -1,7 +1,7 @@
-# Copyright 2020 Jesus Ramiro <jesus@bilbonet.net>
+# Copyright 2022 Jesus Ramiro <jesus@bilbonet.net>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import pet_animal
 from . import pet_animal_type
-from . import hr_employee
+from . import hr_employee_base
 from . import veterinary_appointment
